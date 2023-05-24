d08c6c5f73b69f677a00f73775455af96403e461
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-05-24 15:05:51 +0200

    reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require_relative "../app/models/restaurant"
+require 'active_record'
+
+Restaurant.destroy_all
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+
+puts 'Creating restaurants...'
+10.times do
+  restaurant = Restaurant.new(name: Faker::TvShows::RuPaul.queen, address: Faker::Movies::LordOfTheRings.location, category: CATEGORIES.sample, phone_number: Faker::PhoneNumber.cell_phone_in_e164)
+  restaurant.save!
+end
+
+puts 'Finished!'
